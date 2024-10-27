9be63eb8c8aded4223715b06f43b36ff8ffd96e0
Jorazon <oskari.pahkala@gmail.com>
2024-10-27 13:10:14 +0200

    change fstring quotes back to single

--- resources/save_deck.py
+++ resources/save_deck.py
@@ -49,10 +49,10 @@ def fetch_and_parse_json(url, path):
 	elif len(commanders) > 1 :
 		description += "Commanders:\n"
 	for card_id, card_info in commanders :
-		description += f"{card_info["card"]["name"]}\n"
+		description += f'{card_info["card"]["name"]}\n'
 	if len(description) > 0 :
 		description += "\n"
-	description += f"Author: {json_data["createdByUser"]["userName"]}\nLink: moxfield.com/decks/{json_data["publicId"]}"
+	description += 'Author: {json_data["createdByUser"]["userName"]}\nLink: moxfield.com/decks/{json_data["publicId"]}'
 	comments.appendChild(document.createTextNode(description))
 	deck.appendChild(comments)
 
@@ -84,7 +84,7 @@ def fetch_and_parse_json(url, path):
 	# Regex pattern for invalid characters for filename
 	pattern = r"[\\\/:\*\?\"<>|]"
 	# Create filename
-	xml_file_name = f"{json_data["name"]} [{json_data["publicId"]}].cod"
+	xml_file_name = f'{json_data["name"]} [{json_data["publicId"]}].cod'
 	# Clean filename of invalid characters and concatenate path
 	xml_file_name = path +  re.sub(pattern, "", xml_file_name)
 	# Write file
@@ -92,6 +92,6 @@ def fetch_and_parse_json(url, path):
 		pretty = document.toprettyxml(indent="    ")
 		xml_file.write(pretty)
 
-	print(f"XML file created: {xml_file_name}")
+	print(f'XML file created: {xml_file_name}')
 
 	return xml_file_name
